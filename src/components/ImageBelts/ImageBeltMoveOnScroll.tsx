e7851f10ca2ca31d5ca2ce094ca6ac0cea683788
AxiomaticAdi <arudra@bu.edu>
2025-02-23 01:44:06 -0800

    fix: make right scroll work

--- src/components/ImageBelts/ImageBeltMoveOnScroll.tsx
+++ src/components/ImageBelts/ImageBeltMoveOnScroll.tsx
@@ -28,7 +28,7 @@ export default function ImageBeltMoveOnScroll({
 	const transformStyle =
 		direction === "left"
 			? { transform: `translateX(-${scrollPosition}px)` }
-			: { transform: `translateX(${scrollPosition}px)` };
+			: { transform: `translateX(${scrollPosition - 2500}px)` };
 
 	return (
 		<div className="w-full inline-flex flex-nowrap mt-10">
@@ -36,23 +36,26 @@ export default function ImageBeltMoveOnScroll({
 				className="flex gap-5 items-center justify-center"
 				style={transformStyle}
 			>
-				{images.concat(images).map((image, imageIndex) => (
-					<li
-						key={imageIndex}
-						className={clsx(
-							"relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl",
-							rotations[imageIndex % rotations.length]
-						)}
-					>
-						<Image
-							src={image}
-							alt=""
-							sizes="(min-width: 640px) 18rem, 11rem"
-							height={200}
-							width={300}
-						/>
-					</li>
-				))}
+				{images
+					.concat(images)
+					.concat(images)
+					.map((image, imageIndex) => (
+						<li
+							key={imageIndex}
+							className={clsx(
+								"relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl",
+								rotations[imageIndex % rotations.length]
+							)}
+						>
+							<Image
+								src={image}
+								alt=""
+								sizes="(min-width: 640px) 18rem, 11rem"
+								height={200}
+								width={300}
+							/>
+						</li>
+					))}
 			</ul>
 		</div>
 	);
