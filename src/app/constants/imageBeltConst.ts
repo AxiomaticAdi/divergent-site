337a2d37392636d4ff89a09e1f85b817ec3ee9d8
AxiomaticAdi <arudra@bu.edu>
2025-02-23 01:29:47 -0800

    feat: add second image belt

--- src/app/constants/imageBeltConst.ts
+++ src/app/constants/imageBeltConst.ts
@@ -7,10 +7,18 @@ export const rotations = [
 	"-rotate-2",
 ];
 
-export const actionImages = [
+export const actionImages1 = [
 	"https://res.cloudinary.com/djxfhec23/image/upload/v1740294645/DivergentImprov/ActionShots/476489760_17859705321353346_6629993092432768771_n_ffrld9.jpg",
 	"https://res.cloudinary.com/djxfhec23/image/upload/v1740294645/DivergentImprov/ActionShots/476486959_17859705384353346_9123393268771681481_n_temm62.jpg",
 	"https://res.cloudinary.com/djxfhec23/image/upload/v1740294645/DivergentImprov/ActionShots/476484934_17859705357353346_8508114069373922787_n_juutmu.jpg",
 	"https://res.cloudinary.com/djxfhec23/image/upload/v1740294644/DivergentImprov/ActionShots/476502419_17859705339353346_2139681182932061626_n_sliaqw.jpg",
 	"https://res.cloudinary.com/djxfhec23/image/upload/v1740294644/DivergentImprov/ActionShots/476484962_17859705366353346_6677198411664136124_n_jwsktx.jpg",
+];
+
+export const actionImages2 = [
+	"https://res.cloudinary.com/djxfhec23/image/upload/v1740302340/DivergentImprov/ActionShots2/480898485_17862152910353346_7040838684674823852_n_v8rw6k.jpg",
+	"https://res.cloudinary.com/djxfhec23/image/upload/v1740302340/DivergentImprov/ActionShots2/479170709_17861264412353346_8266730008488768736_n_z5rdjf.jpg",
+	"https://res.cloudinary.com/djxfhec23/image/upload/v1740302340/DivergentImprov/ActionShots2/481164271_17862152847353346_2408745233436388224_n_qteafk.jpg",
+	"https://res.cloudinary.com/djxfhec23/image/upload/v1740302340/DivergentImprov/ActionShots2/479180249_17861264424353346_4591546096383632696_n_bo8lue.jpg",
+	"https://res.cloudinary.com/djxfhec23/image/upload/v1740302340/DivergentImprov/ActionShots2/480898485_17862152910353346_7040838684674823852_n_v8rw6k.jpg",
 ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { actionImages } from "@/app/constants/imageBeltConst";
-import ImageBeltMoving from "@/components/ImageBelts/ImageBeltMoving";
+import { actionImages1, actionImages2 } from "@/app/constants/imageBeltConst";
+import ImageBeltMoveOnScroll from "@/components/ImageBelts/ImageBeltMoveOnScroll";
 import Hero from "@/app/sections/Hero";
 
 export default function Home() {
@@ -9,7 +9,8 @@ export default function Home() {
 				<div className="h-96 w-full border border-zinc-300 justify-center"></div>
 			</div>
 			<Hero />
-			<ImageBeltMoving images={actionImages} />
+			<ImageBeltMoveOnScroll images={actionImages1} direction="left" />
+			<ImageBeltMoveOnScroll images={actionImages2} direction="right" />
 			<div className="max-w-2xl mx-auto flex flex-col gap-5">
 				<div className="h-96 w-full border border-zinc-300 justify-center"></div>
 				<div className="h-96 w-full border border-zinc-300 justify-center"></div>
