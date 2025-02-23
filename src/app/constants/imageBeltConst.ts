12dcddecbff92d187683b1b6df792aa4565db212
AxiomaticAdi <arudra@bu.edu>
2025-02-22 23:13:52 -0800

    feat: create image belts

---
+++ src/app/constants/imageBeltConst.ts
@@ -0,0 +1,7 @@
+export const actionImages = [
+	"https://res.cloudinary.com/djxfhec23/image/upload/v1740294645/DivergentImprov/ActionShots/476489760_17859705321353346_6629993092432768771_n_ffrld9.jpg",
+	"https://res.cloudinary.com/djxfhec23/image/upload/v1740294645/DivergentImprov/ActionShots/476486959_17859705384353346_9123393268771681481_n_temm62.jpg",
+	"https://res.cloudinary.com/djxfhec23/image/upload/v1740294645/DivergentImprov/ActionShots/476484934_17859705357353346_8508114069373922787_n_juutmu.jpg",
+	"https://res.cloudinary.com/djxfhec23/image/upload/v1740294644/DivergentImprov/ActionShots/476502419_17859705339353346_2139681182932061626_n_sliaqw.jpg",
+	"https://res.cloudinary.com/djxfhec23/image/upload/v1740294644/DivergentImprov/ActionShots/476484962_17859705366353346_6677198411664136124_n_jwsktx.jpg",
+];
